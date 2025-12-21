717bbf935dc37e26ea3a03e0e73b0753a4fa086f
neophyte <neophyteplayground@gmail.com>
2025-12-21 17:32:19 +0000

    Bump Custom Security String to 2025-12-05

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.nad.custom_version=2025-11-1
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.nad.custom_version=2025-12-5
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
