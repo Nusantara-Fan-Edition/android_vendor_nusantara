ca1f429a5b83d5d79d791cce7a546bea1be10652
neophyte <168145100+k4ngcaribug@users.noreply.github.com>
2026-01-18 21:42:31 +0700

    Bump Custom Security String to 2026-02-1

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.nad.custom_version=2025-12-5
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.nad.custom_version=2026-02-1
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
